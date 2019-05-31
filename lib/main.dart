ec54a3c68e1744088fdd5cd65db1d3df8b9abcdc
Nadir Bounoua <anbounoua@gmail.com>
2019-05-31 23:35:14 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+// Flutter code sample for material.Card.1
+
+// This sample shows creation of a [Card] widget that shows album information
+// and two actions.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: MyStatelessWidget(),
+      ),
+    );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.image),
+              title: Text('Accident de voiture'),
+              subtitle: Text('Rue, Quartier, Wilaya, Algérie'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Title',
+                labelStyle: TextStyle(color: Colors.blue),
+              ),
+            ),
+            TextFormField(
+              maxLines: null,
+              controller: TextEditingController(),
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
+                labelText: 'Description',
+                labelStyle: TextStyle(color: Colors.blue),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {/* ... */},
+              child:
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(Icons.gps_fixed),
+                    Text('Ajouter ma position')
+                  ],
+                ),
+              color: Colors.blue,
+              textColor: Colors.white,
+            ),
+            ButtonTheme.bar(
+              // make buttons use the appropriate styles for cards
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: const Text('Annuler'),
+                    onPressed: () {/* ... */},
+                  ),
+                  FlatButton(
+                    child: const Text('Poster'),
+                    onPressed: () {/* ... */},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
