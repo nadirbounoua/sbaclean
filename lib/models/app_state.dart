c8b5452e1415f8bfcfc7b8d929c1d36dd8b4d6d0
Aziz <a.attouche@esi-sba.dz>
2019-06-23 02:02:30 +0100

    create AppState

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -1,3 +1,15 @@
+import 'anomaly.dart';
+import 'package:meta/meta.dart';
+import 'package:geolocator/geolocator.dart';
+
+@immutable
 class AppState {
-  
+
+  final List<Anomaly> anomalies;
+
+  final bool chooseCamera;
+
+  Position position;
+
+  AppState({this.anomalies = const [], this.chooseCamera = false, this.position});
 }
