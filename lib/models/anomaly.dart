d033333b0f5f2c8f8c4e94480c2668d8a0acbc94
Aziz <a.attouche@esi-sba.dz>
2019-06-23 02:02:13 +0100

    add fields to Anomaly model

--- lib/models/anomaly.dart
+++ lib/models/anomaly.dart
@@ -1,8 +1,10 @@
  class Anomaly {
   String title;
   String description;
+  String longitude;
+  String latitude;
 
-  Anomaly({this.title, this.description});
+  Anomaly({this.title , this.description, this.longitude,this.latitude });
 
   factory Anomaly.fromJson(Map<String,dynamic> json){
     return Anomaly(description: json['description'] as String,title: json['title'] as String);
