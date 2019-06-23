eefbe1a1e423b8c39c89b61feec90b68dc6655ff
Aziz <a.attouche@esi-sba.dz>
2019-06-23 02:03:46 +0100

    add StoreConnector to feed screen

--- lib/screens/feed/feed.dart
+++ lib/screens/feed/feed.dart
@@ -3,11 +3,40 @@ import '../settings/settings.dart';
 import '../../models/anomaly.dart';
 import '../../backend/api.dart';
 import 'widgets/post_list.dart';
-
-void main() => runApp(Feed());
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:learning2/models/app_state.dart';
+import 'package:learning2/redux/actions.dart';
+import 'package:learning2/redux/reducers.dart';
+//void main() => runApp(Feed());
  
-class Feed extends StatelessWidget {
+class Feed extends StatefulWidget {
+  Feed({Key key}) : super(key: key);
   static const String _title = 'Signalements';
+
+
+  @override
+  _FeedState createState() {
+    // TODO: implement createState
+    return _FeedState();
+  }
+}
+
+class _FeedState extends State<Feed> {
+  bool loading = false;
+  List<Anomaly> list;
+  @override
+  Widget build(BuildContext context){
+    return StoreConnector<AppState,AppState>(
+      converter: (store) =>  store.state,
+      builder: (context,state) {
+
+        return PostList(posts: state.anomalies);
+      },
+    );
+  }
+}
+/*
+class Feed extends StatelessWidget {
   Api api = Api();
   @override
   Widget build(BuildContext context) {
@@ -42,6 +71,6 @@ class Feed extends StatelessWidget {
     );
   }
 }
-
+*/
 
 
