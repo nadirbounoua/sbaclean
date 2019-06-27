cc983a62b69f4e03b96c75ba209cfba202d9137d
Aziz <a.attouche@esi-sba.dz>
2019-06-27 15:10:14 +0100

    add delete & set reaction api calls methods

--- lib/backend/api.dart
+++ lib/backend/api.dart
@@ -1,4 +1,5 @@
 import '../models/anomaly.dart';
+import 'package:learning2/models/reaction.dart';
 import '../projectSettings.dart' as ProjectSettings;
 import 'package:http/http.dart' as http;
 import 'dart:io';
@@ -38,4 +39,30 @@ class Api {
     return responseJson['changed'];
   }
 
+  Future setReactionPost(Anomaly anomaly,Reaction reaction) async {
+    print("API" + reaction.toString());
+
+    var url = ProjectSettings.apiUrl + '/api/v1/posts/reaction/';
+    var response = await http.post(url,
+      body: {'post' : reaction.post.toString(), 
+            'reaction_owner': reaction.reactionOwner.toString(), 
+            'is_like': reaction.isLike.toString()
+            },
+      headers: {HttpHeaders.authorizationHeader: "Token "+ProjectSettings.authToken}
+    );
+    Map<String, dynamic> responseJson = json.decode(response.body);
+    print(responseJson);
+    return responseJson;
+
+  }
+
+  Future deleteReaction(Reaction reaction) async {
+    var url = ProjectSettings.apiUrl + '/api/v1/posts/reaction/'+ reaction.id.toString();
+    var response = await http.delete(url,
+      headers: {HttpHeaders.authorizationHeader: "Token "+ProjectSettings.authToken}
+
+    );
+
+    return response.statusCode;
+  }
 }
