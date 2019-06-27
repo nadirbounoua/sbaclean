c627f89c464d66928e9b1dd4a1c143dbac046608
Aziz <a.attouche@esi-sba.dz>
2019-06-27 21:05:06 +0100

    add reaction parser

--- lib/backend/utils.dart
+++ lib/backend/utils.dart
@@ -1,9 +1,15 @@
 import '../models/anomaly.dart';
+import 'package:learning2/models/reaction.dart';
 import '../projectSettings.dart' as ProjectSettings;
 import 'dart:convert';
-
 List<Anomaly> parsePost(String responseBody){
     final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
     
     return parsed.map<Anomaly>((json) => Anomaly.fromJson(json)).toList();
+}
+
+List<Reaction> parseReaction(String responseBody){
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    
+    return parsed.map<Reaction>((json) => Reaction.fromJson(json)).toList();
 }
