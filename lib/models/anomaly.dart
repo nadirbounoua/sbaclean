a6ae7e166b375fe45c613681c654d1a750ddb2ab
Aziz <a.attouche@esi-sba.dz>
2019-06-27 15:11:37 +0100

    add reactions to Anomaly model

--- lib/models/anomaly.dart
+++ lib/models/anomaly.dart
@@ -1,18 +1,31 @@
+ import 'package:learning2/models/reaction.dart';
+ 
  class Anomaly {
+  int id;
+  int postOwner;
   String title;
   String description;
   String longitude;
   String latitude;
+  List<int> reactions = [];
+  Reaction userReaction;
 
-  Anomaly({this.title , this.description, this.longitude,this.latitude });
+  Anomaly({this.title , this.description, this.longitude,this.latitude,this.id, this.reactions ,this.userReaction, this.postOwner });
 
   factory Anomaly.fromJson(Map<String,dynamic> json){
-    return Anomaly(description: json['description'] as String,title: json['title'] as String);
+    return Anomaly(id:json['id'] as int,
+            description: json['description'] as String,
+            title: json['title'] as String, 
+            longitude: json['longitude'] as String,
+            latitude: json['latitude'] as String,
+            postOwner: json['post_owner'] as int,
+            reactions: json['reactions'].cast<int>() as List<int>
+            );
   }
 
   @override
     String toString() {
       // TODO: implement toString
-      return "Anomaly $description $latitude $longitude $title";
+      return "Anomaly $id $description $latitude $longitude $title";
     }
 }
