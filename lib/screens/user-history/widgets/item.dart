f1f881d5afcfaf45893bf341aaa88ac88de9bb96
Aziz <a.attouche@esi-sba.dz>
2019-07-11 00:05:17 +0100

    add userAnomaliesHistory widgets

---
+++ lib/screens/user-history/widgets/item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:learning2/models/anomaly.dart';
+import 'package:learning2/screens/user-history/widgets/list-tile.dart';
+
+class EntryItem extends StatelessWidget {
+  const EntryItem(this.entry);
+
+  final Entry entry;
+
+  Widget _buildTiles(Entry root) {
+    if (root.children.isEmpty) return ListTile(title: Text(root.title));
+    return ExpansionTile(
+      onExpansionChanged: (expanded) {
+        
+      },
+      leading: Icon(Icons.keyboard_arrow_right),
+      key: PageStorageKey<Entry>(root),
+      title: Text(root.title),
+      children: root.children.map((anomaly) => ListElement(anomaly: anomaly,)).toList(),
+      trailing: Text(''),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(entry);
+  }
+}
+
+
+class Entry {
+  Entry({this.title, this.children = const []});
+
+  String title;
+  List<Anomaly> children;
+}
