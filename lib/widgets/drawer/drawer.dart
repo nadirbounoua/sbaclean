ca57f87dd416e77f64e0f5a37e75ba09cfccb61b
Aziz <a.attouche@esi-sba.dz>
2019-07-10 18:25:02 +0100

    create DrawerWidget

---
+++ lib/widgets/drawer/drawer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
+import 'package:learning2/screens/feed/feed.dart';
+import 'package:learning2/screens/settings/settings.dart';
+
+class DrawerWidget extends StatelessWidget {
+
+  String actualRoute;
+  DrawerWidget({this.actualRoute});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Text("Header"),
+            decoration: BoxDecoration(
+              color: Colors.blue
+            ),  
+          ),
+          ListTile(
+            title: Text("Fil d'actualité"),
+            onTap: () {
+              navigate(context, actualRoute, "Feed", FeedScreen());
+            },
+          ),
+          ListTile(
+            title: Text("Paramétres"),
+            onTap: () {
+              navigate(context, actualRoute, "Settings", Settings());
+
+            },
+          ),
+          ListTile(
+            title: Text("Se déconnecter"),
+          )
+        ],
+      ),
+    );
+  }
+
+  navigate(BuildContext context,String actualRoute, String title, Widget route) {
+    if (actualRoute != title) {
+      Navigator.pop(context);
+      Navigator.push(context, new MaterialPageRoute(builder: (context) => route));
+    }
+  }
+}
