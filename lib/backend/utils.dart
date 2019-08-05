eccf1e872a3349ffad080bf53a568f38a445f8ca
Aziz <a.attouche@esi-sba.dz>
2019-08-05 08:09:37 +0100

    update anomaly & post utils

--- lib/backend/utils.dart
+++ lib/backend/utils.dart
@@ -5,19 +5,37 @@ import 'dart:convert';
 import 'package:sbaclean/models/comment.dart';
 import '../models/anomaly.dart';
 import '../models/user.dart';
-List<Anomaly> parsePost(String responseBody){
+import 'package:sbaclean/models/post.dart';
+
+List<Anomaly> parseAnomalies(String responseBody){
     final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
     
-    return parsed.map<Anomaly>((json) => Anomaly.fromJson(json)).toList();
+    return parsed.map<Anomaly>((json) {
+      
+      return createFromJson(json);
+      }).toList();
 }
+Anomaly createFromJson(dynamic json) {
+      Post post = Post.fromJson(json);
+      Anomaly anomaly = Anomaly.fromJson(json);
+      anomaly.post = post;
+      return anomaly;
+}
+
+Anomaly createFromJsonPost(dynamic json) {
+  print(json.toString());
+      Post post = Post.fromJson(json);
+      Anomaly anomaly = Anomaly.fromJson(json);
+      anomaly.post = post;
+      return anomaly;
+}
+
 
 List<Reaction> parseReaction(String responseBody){
     final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
     
     return parsed.map<Reaction>((json) => Reaction.fromJson(json)).toList();
 }
-
-
 
 
 List<Comment> parseComment(String responseBody){
@@ -37,5 +55,12 @@ User parseOneUser(String responseBody){
 }
 Anomaly parseOneAnomaly(String responseBody){
     final parsed = json.decode(responseBody);
-    return Anomaly.fromJson(parsed);
+
+    return createFromJsonPost(parsed);
+}
+
+Post parseOnePost(String responseBody) {
+  final parsed = jsonDecode(responseBody);
+  
+  return Post.fromJson(parsed);
 }
