cf85a88e414a98efd61ffa2d21f3d471628ec8ac
Aziz <a.attouche@esi-sba.dz>
2019-08-30 20:43:41 +0100

    add loading booleans

--- lib/store/feed_state.dart
+++ lib/store/feed_state.dart
@@ -9,21 +9,28 @@ class FeedState {
   final List<Anomaly> anomalies;
   final bool postsChanged;
   final List<Reaction> userReactions;
-
+  final bool isAnomaliesLoading;
+  final bool isAddAnomalyLoading;
   FeedState({this.anomalies = const [], 
             this.postsChanged = false,
             this.userReactions = const [],
+            this.isAnomaliesLoading = false,
+            this.isAddAnomalyLoading = false
   });
 
   FeedState copyWith({
       List<Anomaly> anomalies,
       bool postsChanged,
       List<Reaction> userReactions,
+      bool isAnomaliesLoading,
+      bool isAddAnomalyLoading,
   }) {
     return FeedState(
       anomalies: anomalies ?? this.anomalies,
       postsChanged: postsChanged ?? this.postsChanged,
-      userReactions: userReactions ?? this.userReactions
+      userReactions: userReactions ?? this.userReactions,
+      isAnomaliesLoading: isAnomaliesLoading ?? this.isAnomaliesLoading,
+      isAddAnomalyLoading: isAddAnomalyLoading ?? this.isAddAnomalyLoading
     );
   }
 }
