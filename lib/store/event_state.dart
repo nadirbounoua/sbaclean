7e9ba6d50fda2e3be39ea83172b9116845a10bde
Aziz <a.attouche@esi-sba.dz>
2019-09-05 02:47:04 +0100

    add loadings to event state

--- lib/store/event_state.dart
+++ lib/store/event_state.dart
@@ -6,15 +6,19 @@ import 'package:meta/meta.dart';
 class EventState {
 
   final List<Event> events;
+  final bool isEventsLoading;
 
-
-  EventState({this.events = const []});
+  EventState({this.events = const [],
+            this.isEventsLoading =false
+  });
 
   EventState copyWith({
     List<Event> events,
+    bool isEventsLoading,
   }) {
     return EventState(
-        events: events ?? this.events
+        events: events ?? this.events,
+        isEventsLoading: isEventsLoading ?? this.isEventsLoading
     );
   }
 
