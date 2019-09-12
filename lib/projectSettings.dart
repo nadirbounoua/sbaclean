b33cd6de39009322bc2ac34ef900c9c0cfcc58c4
Aziz <a.attouche@esi-sba.dz>
2019-09-12 19:33:18 +0100

    add onesignal app id variable

--- lib/projectSettings.dart
+++ lib/projectSettings.dart
@@ -1,5 +1,7 @@
-String apiUrl = "http://192.168.1.36:8000";
+String apiUrl = "http://192.168.43.92:8000";
 // auth token
 String authToken = "485d5e2f79512be1280a9f82b8b95c3ecb934bf4";
 
 String imageServer = "https://image-cloudinary.herokuapp.com/photos";
+
+String oneSignalAppId;
