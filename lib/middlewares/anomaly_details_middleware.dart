0e5e3ecfe6227a6650fb6dadb8c743a6e1053662
Aziz <a.attouche@esi-sba.dz>
2019-09-16 01:52:43 +0100

    fix notification botton bug

--- lib/middlewares/anomaly_details_middleware.dart
+++ lib/middlewares/anomaly_details_middleware.dart
@@ -1,5 +1,6 @@
 import 'package:redux/redux.dart';
 import 'package:sbaclean/actions/anomaly_details_actions.dart';
+import 'package:sbaclean/actions/feed_actions.dart';
 import 'dart:convert';
 import 'package:sbaclean/backend/api.dart';
 import 'package:sbaclean/backend/utils.dart';
@@ -30,6 +31,9 @@ getReaction() {
           .getUserPostReaction(action.anomaly.post.id, store.state.auth.user.id)
             .then((response) {
                 action.anomaly.post.userReaction = parseOneReaction(response);
+                store.dispatch(new SetPostsChanged(changed: false));
+
+                store.dispatch(SilentAddAnomalyAction(anomaly: action.anomaly));
             });
 
   };
@@ -52,7 +56,6 @@ addComment() {
     await api.copyWith(store.state.auth.user.authToken)
               .createComment(action.item)
                 .then((response) {
-                  print(response);
                   addCommentHelper(store, response);
               });
   };
